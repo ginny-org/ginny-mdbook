49f260df8733cfba61bae7494065cee0d9559f04
Jesse van den Kieboom <JKieboom@esri.com>
2023-09-28 10:32:18 +0200

    Add github style quote types

--- lib/index.tsx
+++ lib/index.tsx
@@ -15,6 +15,12 @@ marked.use(gfmHeadingId());
 const mathMagicPrefix = "__math__:";
 const mathMagicSuffix = ":__math__";
 
+const icons = {
+  note: `<svg class="note" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path></svg>`,
+  important: `<svg class="important" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path></svg>`,
+  warning: `<svg class="warning" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path></svg>`
+};
+
 marked.use({
   tokenizer: {
     codespan(src) {
@@ -74,6 +80,26 @@ marked.use({
       }
 
       return `<code>${code}</code>`;
+    },
+
+    blockquote: (quote) => {
+      const alert = quote.match(/^<p>\[!(NOTE|IMPORTANT|WARNING)\]([\s\S]*)<\/p>$/m);
+      const type = alert?.[1]?.toLowerCase();
+
+      if (alert && isAlertType(type)) {
+        const typeTitle = type[0].toUpperCase() + type.slice(1);
+
+        return `
+        <div class="alert ${type}">
+          <p>
+            <span class="alert-title">${icons[type]} ${typeTitle}</span>
+            <br>
+            ${alert[2]}
+          </p>
+        </div>`;
+      }
+
+      return `<blockquote>${quote}</blockquote>`;
     },
 
     code: (code, language) => {
@@ -346,6 +372,19 @@ class HeadingTracker {
     heading.id = id;
     this.ids.add(id);
   }
+}
+
+type AlertType = "note" | "important" | "warning";
+
+function isAlertType(type: string | undefined): type is AlertType {
+  switch (type) {
+    case "note":
+    case "important":
+    case "warning":
+      return true;
+  }
+
+  return false;
 }
 
 /**
